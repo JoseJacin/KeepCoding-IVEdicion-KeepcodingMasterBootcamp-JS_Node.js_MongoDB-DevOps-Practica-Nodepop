7aa8189d809eee21426d18b790f488f4764e4d0b
José Sánchez <joseynene@hotmail.com>
2016-11-30 01:52:21 +0100

    Create tags.js

---
+++ routes/apiv1/tags.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var Anuncio = require ('../../models/Anuncio.js');
+//Modulo de error
+var errorHandler = require ('../../utils/Error.js').error;
+
+//Recuperar solo los tags
+router.get('/', function(req, res) {
+    Anuncio.listaTags( function(err, lista) {
+        if (err) {
+            return errorHandler(err,res);
+        }
+        res.json({ok:true, data: lista});
+    });
+});
+module.exports = router;
